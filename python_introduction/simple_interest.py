cf21b76d37f7ad9b176caeb2206ceb6adafebd2f
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-09-10 12:00:31 +0300

    Rename principal correctly

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,9 @@
-principle = 1000
+principal = 1000
 rate = 0.05
 time = 3
 
 # Calculate interest
-interest = principle*rate*time
+interest = principal*rate*time
 
 #Results
 print("The simple interest is:", interest)
