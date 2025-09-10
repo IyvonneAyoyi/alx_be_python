f552278719a03c0a9cac052ca40d54b55f8615f9
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-09-10 11:49:17 +0300

    Basic mathematical operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+#Perform operations
+total = number1+number2
+difference = number1-number2
+product = number1*number2
+
+#Results
+print("Addition of number1 and number2 is", total)
+print("Subtraction of number1 and number2 is", difference)
+print("Multiplication of number1 and number2 is:", product)
